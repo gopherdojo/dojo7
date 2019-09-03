d0d2f02158c862eabcb91fe3d935ae99119443c2
Kazufumi Suzuki <skazufumi.a02525@gmail.com>
2019-09-03 20:57:33 +0900

    mainを一旦きれいに

--- kadai1/su-san/main.go
+++ kadai1/su-san/main.go
@@ -2,32 +2,8 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"image/png"
-	"image/jpeg"
 )
 
 func main(){
-
-	f, err := os.Open("test_img/test_img.png")
-
-	if err != nil {
-		fmt.Println("error")
-	}
-	defer f.Close()
-
-	fmt.Printf("%T\n", f)
-
-	img, err := png.Decode(f)
-	if err != nil {
-		fmt.Println("cannot decode")
-	}
-
-	fmt.Println(img.Bounds())
-	fmt.Printf("%T\n", img.Bounds())
-	fmt.Printf("%T\n", img)
-
-	outputFile, err := os.Create("output/output.jpeg")
-
-	jpeg.Encode(outputFile, img, nil)
+	fmt.Println("test")
 }
