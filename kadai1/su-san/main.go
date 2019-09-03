8187cb1c8c0a8102df318a737a3a120161d19306
Kazufumi Suzuki <skazufumi.a02525@gmail.com>
2019-09-03 21:57:57 +0900

    オプション用のコード追加

--- kadai1/su-san/main.go
+++ kadai1/su-san/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/gopherdojo/dojo7/kadai1/su-san/image"
 )
 
-func main(){
+// オプション用の返還前後の拡張子
+// var inputExt = flag.String("i", "jpg", " extension to be converted ")
+// var outputExt = flag.String("o", "png", " extension after conversion")
+
+func main() {
+
+
 	fmt.Println("test")
 	convExts := image.NewConvExts("", "")
 	err := image.FmtConv("test_img/test_img.jpg", convExts)
