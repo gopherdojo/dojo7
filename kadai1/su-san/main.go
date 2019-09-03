eb9bd148e587468234ea2c02123e7d0cbb9f9e3d
Kazufumi Suzuki <skazufumi.a02525@gmail.com>
2019-09-03 22:20:53 +0900

    再帰的に変換後のファイルを生成

--- kadai1/su-san/main.go
+++ kadai1/su-san/main.go
@@ -3,21 +3,57 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 
 	"github.com/gopherdojo/dojo7/kadai1/su-san/image"
 )
 
-// オプション用の返還前後の拡張子
-// var inputExt = flag.String("i", "jpg", " extension to be converted ")
-// var outputExt = flag.String("o", "png", " extension after conversion")
-
 func main() {
 
-
-	fmt.Println("test")
-	convExts := image.NewConvExts("", "")
-	err := image.FmtConv("test_img/test_img.jpg", convExts)
-	if err != nil {
-		fmt.Println(err)
+	// Usage文言
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, `Usage :
+   convimg target_directory`)
+		flag.PrintDefaults()
 	}
+
+	flag.Parse()
+	args := flag.Args()
+
+	if flag.NArg() == 0 {
+		flag.Usage()
+		return
+	}
+
+	targetDir := args[0]
+	targetFiles := dirwalk(targetDir)
+
+	for _, f := range targetFiles {
+		convExts := image.NewConvExts("", "")
+		err := image.FmtConv(f, convExts)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
+// dirwalk は与えられたディレクト以下のファイルパスをリストで返します
+func dirwalk(dir string) []string {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		panic(err)
+	}
+
+	var paths []string
+	for _, file := range files {
+		if file.IsDir() {
+			paths = append(paths, dirwalk(filepath.Join(dir, file.Name()))...)
+			continue
+		}
+		paths = append(paths, filepath.Join(dir, file.Name()))
+	}
+
+	return paths
 }
