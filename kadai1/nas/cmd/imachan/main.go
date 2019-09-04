40bd1cbc7ab8f61c45a5b154bcfc3d47f1b8e82b
yukihiro.taniguchi <taniguchi@liquidinc.asia>
2019-09-05 01:26:31 +0900

    基本機能の実装

--- kadai1/nas/cmd/imachan/main.go
+++ kadai1/nas/cmd/imachan/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "github.com/gopherdojo/dojo7/kadai1/nas/pkg/imachan"
+import (
+	"flag"
+	"log"
+	"path/filepath"
+
+	"github.com/gopherdojo/dojo7/kadai1/nas/pkg/imachan"
+)
 
 func main() {
-	imachan.Sample()
+	err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	flag.Parse()
+	args := flag.Args()
+	for _, arg := range args {
+		path, err := filepath.Abs(arg)
+		if err != nil {
+			return err
+		}
+		err = imachan.Convert(path, "jpg", "png")
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
--- kadai1/nas/pkg/imachan/imachan.go
+++ kadai1/nas/pkg/imachan/imachan.go
@@ -1,6 +1,31 @@
 package imachan
 
-// Sample ...
-func Sample() {
-	println("imachan is the command to convert images")
+import (
+	"fmt"
+	"image"
+	_ "image/jpeg" // to convert jpeg file
+	"image/png"
+	"os"
+	"path/filepath"
+)
+
+// Convert ...
+func Convert(fromPath, fromFormat, toFormat string) error {
+	f, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	fromImg, _, err := image.Decode(f)
+	if err != nil {
+		return err
+	}
+	toPath := fromPath[0:len(fromPath)-len(filepath.Ext(fromPath))] + ".png"
+	toImg, err := os.Create(toPath)
+	err = png.Encode(toImg, fromImg)
+	if err != nil {
+		return err
+	}
+	fmt.Println("success:", fromPath, "->", toPath)
+	return nil
 }
