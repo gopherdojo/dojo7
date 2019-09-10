c0e30728b15352c7401e24609a7d9a56a5766b53
yukihiro.taniguchi <taniguchi@liquidinc.asia>
2019-09-10 23:50:54 +0900

    NewConfig にテストを追加

--- kadai1/nas/pkg/imachan/imachan_test.go
+++ kadai1/nas/pkg/imachan/imachan_test.go
@@ -1,1 +1,64 @@
 package imachan
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestNewConfig(t *testing.T) {
+	tests := []struct {
+		name          string
+		fromFormatStr string
+		toFormatStr   string
+		expected      *Config
+	}{
+		{
+			name:          "PngToJpg",
+			fromFormatStr: "png",
+			toFormatStr:   "jpg",
+			expected: &Config{
+				FromFormat: PngFormat,
+				ToFormat:   JpgFormat,
+			},
+		},
+		{
+			name:          "JpgToPng",
+			fromFormatStr: "png",
+			toFormatStr:   "jpg",
+			expected: &Config{
+				FromFormat: PngFormat,
+				ToFormat:   JpgFormat,
+			},
+		},
+		{
+			name:          "UndefindFromFormat",
+			fromFormatStr: "undefind",
+			toFormatStr:   "jpg",
+			expected:      nil,
+		},
+		{
+			name:          "UndefindToFormat",
+			fromFormatStr: "png",
+			toFormatStr:   "undefind",
+			expected:      nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c, err := NewConfig("", tt.fromFormatStr, tt.toFormatStr)
+			if strings.HasPrefix(tt.name, "Undefind") {
+				if err == nil {
+					t.Errorf("NewConfig(\"\", %s, %s) => %v, want %v", tt.fromFormatStr, tt.toFormatStr, tt.expected, c)
+				}
+				return
+			}
+			if c.FromFormat != tt.expected.FromFormat {
+				t.Errorf("NewConfig(\"\", %s, %s) => %v, want %v", tt.fromFormatStr, tt.toFormatStr, tt.expected, c)
+			}
+			if c.ToFormat != tt.expected.ToFormat {
+				t.Errorf("NewConfig(\"\", %s, %s) => %v, want %v", tt.fromFormatStr, tt.toFormatStr, tt.expected, c)
+			}
+		})
+	}
+}
