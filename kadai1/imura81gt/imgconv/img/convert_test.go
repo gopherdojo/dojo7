e4cdad0167386560e0aef62246229f95892e993c
imura81gt <imura81gt@gmail.com>
2019-09-16 23:32:14 +0900

    add test for img.Enable, img.IsImage

---
+++ kadai1/imura81gt/imgconv/img/convert_test.go
@@ -0,0 +1,123 @@
+package img
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestEnable(t *testing.T) {
+	testcases := []struct {
+		caseName string
+		iType    ImageType
+		expected []string
+	}{
+		{
+			caseName: "Png: true",
+			iType:    ImageType{Png: true, Jpeg: false, Gif: false},
+			expected: []string{"png"},
+		},
+		{
+			caseName: "Jpeg: true",
+			iType:    ImageType{Png: false, Jpeg: true, Gif: false},
+			expected: []string{"jpeg"},
+		},
+		{
+			caseName: "Gif: true",
+			iType:    ImageType{Png: false, Jpeg: false, Gif: true},
+			expected: []string{"gif"},
+		},
+		{
+			caseName: "Png: true, Jpeg: true",
+			iType:    ImageType{Png: true, Jpeg: true, Gif: false},
+			expected: []string{"png", "jpeg"},
+		},
+		{
+			caseName: "Jpeg: true, Gif: true",
+			iType:    ImageType{Png: false, Jpeg: true, Gif: true},
+			expected: []string{"jpeg", "gif"},
+		},
+		{
+			caseName: "Png: true, Jpeg: true, Gif: true",
+			iType:    ImageType{Png: true, Jpeg: true, Gif: true},
+			expected: []string{"png", "jpeg", "gif"},
+		},
+	}
+
+	for _, tc := range testcases {
+		tc := tc // capture range variable. need to set when run parallel test.
+
+		t.Run(tc.caseName, func(t *testing.T) {
+			t.Parallel()
+			actual := tc.iType.Enable()
+			if !reflect.DeepEqual(actual, tc.expected) {
+				t.Errorf("\ncaseName:%s\nactual:%+v\niExpected:%+v\n",
+					tc.caseName,
+					actual,
+					tc.expected,
+				)
+			}
+		})
+	}
+}
+
+func TestAllImageFiles(t *testing.T) {
+
+}
+
+func TestExpectedImageFiles(t *testing.T) {
+
+}
+
+func TestIsImage(t *testing.T) {
+	testcases := []struct {
+		caseName string
+		path     string
+		expected bool
+	}{
+		{
+			caseName: "gif file is image file.",
+			path:     "../testdata/gLenna.gif",
+			expected: true,
+		},
+		{
+			caseName: "jpeg file is image file.",
+			path:     "../testdata/jLenna.jpg",
+			expected: true,
+		},
+		{
+			caseName: "png file is image file.",
+			path:     "../testdata/pLenna.png",
+			expected: true,
+		},
+		{
+			caseName: "empty file is not image file",
+			path:     "../testdata/this_is_not_image.gif",
+			expected: false,
+		},
+	}
+
+	for _, tc := range testcases {
+		tc := tc // capture range variable. need to set when run parallel test.
+
+		t.Run(tc.caseName, func(t *testing.T) {
+			t.Parallel()
+			actual := IsImage(tc.path)
+			if !(actual == tc.expected) {
+				t.Errorf("\ncaseName:%s\nactual:%+v\niExpected:%+v\n",
+					tc.caseName,
+					actual,
+					tc.expected,
+				)
+			}
+		})
+	}
+
+}
+
+func TestIsExpectedImage(t *testing.T) {
+
+}
+
+func TestConvert(t *testing.T) {
+
+}
