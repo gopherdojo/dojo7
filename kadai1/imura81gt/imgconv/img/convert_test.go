d334295a5a78749674991e9c258ee00e34966e4a
imura81gt <imura81gt@gmail.com>
2019-09-18 00:15:44 +0900

    add test

--- kadai1/imura81gt/imgconv/img/convert_test.go
+++ kadai1/imura81gt/imgconv/img/convert_test.go
@@ -50,7 +50,7 @@ func TestEnable(t *testing.T) {
 			t.Parallel()
 			actual := tc.iType.Enable()
 			if !reflect.DeepEqual(actual, tc.expected) {
-				t.Errorf("\ncaseName:%s\nactual:%+v\niExpected:%+v\n",
+				t.Errorf("\ncaseName:%s\nactual:%+v\nExpected:%+v\n",
 					tc.caseName,
 					actual,
 					tc.expected,
@@ -65,6 +65,86 @@ func TestAllImageFiles(t *testing.T) {
 }
 
 func TestExpectedImageFiles(t *testing.T) {
+	testcases := []struct {
+		caseName string
+		files    []string
+		iType    ImageType
+		expected []string
+	}{
+		{
+			caseName: "png is expected image type.",
+			files: []string{
+				"../testdata/pLenna.png",
+				"../testdata/jLenna.jpg",
+				"../testdata/gLenna.gif",
+				"../testdata/chdir/pLenna.png",
+				"../testdata/chdir/jLenna.jpeg",
+				"../testdata/chdir/gLenna.gif",
+				"../testdata/this_is_not_image.png",
+				"../testdata/this_is_not_image.jpg",
+				"../testdata/this_is_not_image.gif",
+			},
+			iType: ImageType{Png: true, Jpeg: false, Gif: false},
+			expected: []string{
+				"../testdata/pLenna.png",
+				"../testdata/chdir/pLenna.png",
+			},
+		},
+		{
+			caseName: "jpg is expected image type.",
+			files: []string{
+				"../testdata/pLenna.png",
+				"../testdata/jLenna.jpg",
+				"../testdata/gLenna.gif",
+				"../testdata/chdir/pLenna.png",
+				"../testdata/chdir/jLenna.jpeg",
+				"../testdata/chdir/gLenna.gif",
+				"../testdata/this_is_not_image.png",
+				"../testdata/this_is_not_image.jpg",
+				"../testdata/this_is_not_image.gif",
+			},
+			iType: ImageType{Png: false, Jpeg: true, Gif: false},
+			expected: []string{
+				"../testdata/jLenna.jpg",
+				"../testdata/chdir/jLenna.jpeg",
+			},
+		},
+		{
+			caseName: "gif is expected image type.",
+			files: []string{
+				"../testdata/pLenna.png",
+				"../testdata/jLenna.jpg",
+				"../testdata/gLenna.gif",
+				"../testdata/chdir/pLenna.png",
+				"../testdata/chdir/jLenna.jpeg",
+				"../testdata/chdir/gLenna.gif",
+				"../testdata/this_is_not_image.png",
+				"../testdata/this_is_not_image.jpg",
+				"../testdata/this_is_not_image.gif",
+			},
+			iType: ImageType{Png: false, Jpeg: false, Gif: true},
+			expected: []string{
+				"../testdata/gLenna.gif",
+				"../testdata/chdir/gLenna.gif",
+			},
+		},
+	}
+
+	for _, tc := range testcases {
+		tc := tc // capture range variable. need to set when run parallel test.
+
+		t.Run(tc.caseName, func(t *testing.T) {
+			t.Parallel()
+			actual := ExpectedImageFiles(tc.files, tc.iType)
+			if !reflect.DeepEqual(actual, tc.expected) {
+				t.Errorf("\ncaseName:%s\nactual:%+v\nExpected:%+v\n",
+					tc.caseName,
+					actual,
+					tc.expected,
+				)
+			}
+		})
+	}
 
 }
 
@@ -108,7 +188,7 @@ func TestIsImage(t *testing.T) {
 			t.Parallel()
 			actual := IsImage(tc.path)
 			if !(actual == tc.expected) {
-				t.Errorf("\ncaseName:%s\nactual:%+v\niExpected:%+v\n",
+				t.Errorf("\ncaseName:%s\nactual:%+v\nExpected:%+v\n",
 					tc.caseName,
 					actual,
 					tc.expected,
@@ -120,6 +200,72 @@ func TestIsImage(t *testing.T) {
 }
 
 func TestIsExpectedImage(t *testing.T) {
+	testcases := []struct {
+		caseName string
+		path     string
+		iType    ImageType
+		expected bool
+	}{
+		{
+			caseName: "png file is expected image file.",
+			path:     "../testdata/pLenna.png",
+			iType:    ImageType{Png: true, Jpeg: false, Gif: false},
+			expected: true,
+		},
+		{
+			caseName: "jpeg file is expected image file.",
+			path:     "../testdata/jLenna.jpg",
+			iType:    ImageType{Png: false, Jpeg: true, Gif: false},
+			expected: true,
+		},
+		{
+			caseName: "gif file is expected image file.",
+			path:     "../testdata/gLenna.gif",
+			iType:    ImageType{Png: false, Jpeg: false, Gif: true},
+			expected: true,
+		},
+
+		{
+			caseName: "png file is unexpected image file.",
+			path:     "../testdata/pLenna.png",
+			iType:    ImageType{Png: false, Jpeg: true, Gif: true},
+			expected: false,
+		},
+		{
+			caseName: "jpeg file is unexpected image file.",
+			path:     "../testdata/jLenna.jpg",
+			iType:    ImageType{Png: true, Jpeg: false, Gif: true},
+			expected: false,
+		},
+		{
+			caseName: "gif file is unexpected image file.",
+			path:     "../testdata/gLenna.gif",
+			iType:    ImageType{Png: true, Jpeg: true, Gif: false},
+			expected: false,
+		},
+
+		{
+			caseName: "does not existed.",
+			path:     "../testdata/does_not_existed.gif",
+			expected: false,
+		},
+	}
+
+	for _, tc := range testcases {
+		tc := tc // capture range variable. need to set when run parallel test.
+
+		t.Run(tc.caseName, func(t *testing.T) {
+			t.Parallel()
+			actual := IsExpectedImage(tc.path, tc.iType)
+			if !(actual == tc.expected) {
+				t.Errorf("\ncaseName:%s\nactual:%+v\nExpected:%+v\n",
+					tc.caseName,
+					actual,
+					tc.expected,
+				)
+			}
+		})
+	}
 
 }
 
