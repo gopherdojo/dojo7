add4a80beb2ef04dd6132d938ce1db770021e9be
waytkheming <kawamura.estyle@gmail.com>
2019-09-18 02:15:58 +0900

    add test code

---
+++ kadai1/waytkheming/cli/cli_test.go
@@ -0,0 +1,24 @@
+package cli_test
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"github.com/waytkheming/godojo/dojo7/kadai1/waytkheming/cli"
+)
+
+func TestCLI_Run(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	c := cli.NewCLI(outStream, errStream)
+	args := strings.Split("convert ./../testdata", " ")
+	exitCode := c.Run(args)
+
+	if exitCode != cli.ExitCodeOK {
+		t.Errorf("failed cli run, exit_code: %d", exitCode)
+	}
+
+	if errStream.Len() > 0 {
+		t.Errorf("failed cli run, output: %q", errStream.String())
+	}
+}
