463b0891a51ad12cf11f1085236ca520ada84fc4
asuke.yasukuni <asuke.yasukuni@ieosamu.com>
2019-09-21 14:37:57 +0900

    add base from type game

--- kadai3_1/main.go
+++ kadai3_1/main.go
@@ -4,18 +4,44 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"math/rand"
 	"os"
+	"time"
 )
 
 func main() {
 	ch := input(os.Stdin)
-	for {
-		text := <-ch
-		fmt.Print(">")
-		fmt.Println(text)
+	timeUp := time.After(5 * time.Second)
 
-		if text == "exit" {
-			break
+	t := time.Now().UnixNano()
+	rand.Seed(t)
+
+	words := []string{
+		"tanuki",
+		"gouge",
+		"english",
+	}
+	fmt.Println(words)
+
+	var sucsess int
+
+L:
+	for {
+		i := rand.Intn(3)
+		selectWord := words[i]
+		fmt.Println(words[i])
+		select {
+		case text := <-ch:
+			if selectWord == text {
+				sucsess++
+				fmt.Println("OK")
+			} else {
+				fmt.Println("NG")
+			}
+		case <-timeUp:
+			fmt.Println("time up")
+			fmt.Println("clear count", sucsess)
+			break L
 		}
 	}
 
@@ -23,14 +49,11 @@ func main() {
 
 func input(r io.Reader) <-chan string {
 	ch := make(chan string)
+
 	go func() {
 		s := bufio.NewScanner(r)
 		for s.Scan() {
 			ch <- s.Text()
-
-			if s.Text() == "exit" {
-				break
-			}
 		}
 		close(ch)
 	}()
