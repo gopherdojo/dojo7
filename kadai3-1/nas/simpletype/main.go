0f74f7f5bee35b5669317e64767128aa68204134
yukihiro.taniguchi <taniguchi@liquidinc.asia>
2019-09-22 13:16:27 +0900

    動く状態

--- kadai3-1/nas/simpletype/main.go
+++ kadai3-1/nas/simpletype/main.go
@@ -1,5 +1,71 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+// Exercise has questions and now question number
+type Exercise struct {
+	Questions      []string
+	NowQuestionNum int
+}
+
+// Next return Question stil has questions or not
+func (e *Exercise) Next() bool {
+	if e.NowQuestionNum >= len(e.Questions) {
+		e.NowQuestionNum++
+		return false
+	}
+	e.NowQuestionNum++
+	return true
+}
+
+// Get return question
+func (e *Exercise) Get() string {
+	if e.NowQuestionNum == 0 {
+		return ""
+	}
+	if e.NowQuestionNum > len(e.Questions) {
+		return ""
+	}
+	return e.Questions[e.NowQuestionNum-1]
+}
+
 func main() {
-	println("simpletype")
+	run()
+}
+
+func run() {
+	bc := context.Background()
+	t := 10 * time.Second
+	ctx, cancel := context.WithTimeout(bc, t)
+	defer cancel()
+	e := &Exercise{
+		Questions: []string{"apple", "banana", "cat", "dog", "egg", "fish"},
+	}
+	if !e.Next() {
+		fmt.Printf("no question")
+		return
+	}
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println(ctx.Err())
+			return
+		default:
+			q := e.Get()
+			fmt.Printf("%s > ", q)
+			var a string
+			fmt.Scan(&a)
+			if a != q {
+				break
+			}
+			if !e.Next() {
+				fmt.Println("completed!")
+				return
+			}
+		}
+	}
 }
