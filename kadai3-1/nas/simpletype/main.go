672a68774e427e8e8f738056d3b87bc4a7e603f6
yukihiro.taniguchi <taniguchi@liquidinc.asia>
2019-09-23 14:14:59 +0900

    問題時間を分離

--- kadai3-1/nas/simpletype/main.go
+++ kadai3-1/nas/simpletype/main.go
@@ -3,31 +3,33 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/gopherdojo/dojo7/kadai3-1/nas/simpletype/pkg/exercise"
 )
 
 func main() {
-	run()
-}
-
-func run() {
-	bc := context.Background()
-	t := 10 * time.Second
-	ctx, cancel := context.WithTimeout(bc, t)
-	defer cancel()
 	e := &exercise.Exercise{
 		Questions: []string{"apple", "banana", "cat", "dog", "egg", "fish"},
 	}
+	t := 10 * time.Second
+	run(e, t)
+}
+
+func run(e *exercise.Exercise, t time.Duration) {
+	bc := context.Background()
+	ctx, cancel := context.WithTimeout(bc, t)
+	defer cancel()
 	if !e.Next() {
-		fmt.Printf("no question")
+		fmt.Fprintf(os.Stderr, "問題がありません")
 		return
 	}
+	var correct int
 	for {
 		select {
 		case <-ctx.Done():
-			fmt.Println(ctx.Err())
+			fmt.Fprintf(os.Stderr, "終了 : 正解数 >>  %d\n", correct)
 			return
 		default:
 			q := e.Get()
@@ -37,9 +39,9 @@ func run() {
 			if a != q {
 				break
 			}
+			correct++
 			if !e.Next() {
-				fmt.Println("completed!")
-				return
+				cancel()
 			}
 		}
 	}
