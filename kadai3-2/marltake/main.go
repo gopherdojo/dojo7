f89d7362163d745837fdf6435b3e7460f0527920
marltake <otake.shigenori@gmail.com>
2019-09-24 10:22:50 +0900

    wip

--- kadai3-2/marltake/main.go
+++ kadai3-2/marltake/main.go
@@ -9,8 +9,9 @@ func main() {
 	var url string
 	var cpus int
 	flag.StringVar(&url, "url", "", "download target")
-	flag.IntVar(&cpus, "p", 1, "number of parallel downloading")
+	flag.IntVar(&cpus, "p", 4, "number of parallel downloading")
 	flag.Parse()
+	url = "http://ftp.riken.jp/Linux/ubuntu-releases/bionic/ubuntu-18.04.3-live-server-amd64.iso"
 	if url == "" {
 		return
 	}
--- kadai3-2/marltake/pget/pget.go
+++ kadai3-2/marltake/pget/pget.go
@@ -1,6 +1,27 @@
 package pget
 
-func Download(url string, cpus int)  {
-	println(url)
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"path"
+)
+
+func Download(url string, cpus int) {
+	saveFile := path.Base(url)
+	res, err := http.DefaultClient.Head(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if res.Header.Get("Accept-Ranges") != "bytes" {
+		log.Fatal("Target url don't accept Ranges.")
+	}
+	tempfile, err := ioutil.TempFile("", saveFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tempfile.Close()
+	println(tempfile.Name())
+	println(res.ContentLength)
 	println(cpus)
 }
