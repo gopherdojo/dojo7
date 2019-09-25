7ce6e348f0cf5ba9cbc465761af7e109385273ea
nas <nas.silver.bullet@gmail.com>
2019-09-26 01:18:22 +0900

    ロジックを修正

--- kadai4/nas/fortune/fortune.go
+++ kadai4/nas/fortune/fortune.go
@@ -1,6 +1,10 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 const (
 	// Great lack type
@@ -28,14 +32,14 @@ func (f DateFunc) Now() time.Time {
 
 // Parameter behave random
 type Parameter interface {
-	Random() float64
+	Float64() float64
 }
 
 // ParameterFunc return double number
 type ParameterFunc func() float64
 
-// Random return float64
-func (f ParameterFunc) Random() float64 {
+// Float64 return float64
+func (f ParameterFunc) Float64() float64 {
 	return f()
 }
 
@@ -45,18 +49,19 @@ type Fortune struct {
 	Parameter
 }
 
-// Lack has any lack type
-type Lack struct {
-	Type string
-}
-
 // Draw return random Fortune
-func (f *Fortune) Draw() (*Lack, error) {
+func (f *Fortune) Draw() (string, error) {
 	d := today(f.Date)
 	if isNewYear(d) {
-		return &Lack{Great}, nil
+		return Great, nil
 	}
-	return &Lack{Great}, nil
+
+	p := random(f.Parameter)
+	l, err := draw(p)
+	if err != nil {
+		return "", err
+	}
+	return l, nil
 }
 
 func today(d Date) time.Time {
@@ -76,4 +81,25 @@ func isNewYear(d time.Time) bool {
 		2: true,
 		3: true,
 	}[day]
+}
+
+func random(p Parameter) float64 {
+	if p == nil {
+		return rand.Float64()
+	}
+	return p.Float64()
+}
+
+func draw(p float64) (string, error) {
+	switch {
+	case 6.0/6.0 >= p && p > 5.0/6.0:
+		return Great, nil
+	case 5.0/6.0 >= p && p > 3.0/6.0:
+		return High, nil
+	case 3.0/6.0 >= p && p > 1.0/6.0:
+		return Middle, nil
+	case 1.0/6.0 >= p && p >= 0.0/6.0:
+		return Low, nil
+	}
+	return "", fmt.Errorf("Can't draw, please redraw")
 }
--- kadai4/nas/fortune/fortune_test.go
+++ kadai4/nas/fortune/fortune_test.go
@@ -5,22 +5,55 @@ import (
 	"time"
 )
 
-func TestDraw(t *testing.T) {
+func TestFortuneDraw(t *testing.T) {
 	cases := []struct {
 		name      string
 		date      time.Time
-		Parameter float64
+		parameter float64
 		want      string
 	}{
-		{"Great", time.Date(2019, 9, 25, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 60*60*9)), 1.00, Great},
+		{"Low", time.Date(2019, 9, 25, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 60*60*9)), 0.16, Low},
+		{"Middle", time.Date(2019, 9, 25, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 60*60*9)), 0.5, Middle},
+		{"High", time.Date(2019, 9, 25, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 60*60*9)), 0.83, High},
+		{"Great", time.Date(2019, 9, 25, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 60*60*9)), 1, Great},
+		{"NewYear", time.Date(2019, 1, 1, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 60*60*9)), 0, Great},
 	}
 
 	for _, tt := range cases {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Great; tt.want != got {
-				t.Errorf("Draw() => %s, but want %s", got, tt.want)
+			f := &Fortune{
+				Date:      DateFunc(func() time.Time { return tt.date }),
+				Parameter: ParameterFunc(func() float64 { return tt.parameter }),
+			}
+			got, err := f.Draw()
+			if err != nil {
+				t.Errorf("Unexpected Error : %v", err)
+			}
+			if tt.want != got {
+				t.Errorf("Draw() => want %s, but got %s", tt.want, got)
 			}
 		})
+	}
+}
+
+func TestFortuneDrawError(t *testing.T) {
+	test := struct {
+		name      string
+		date      time.Time
+		parameter float64
+		want      string
+	}{"Invalid", time.Date(2019, 9, 25, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 60*60*9)), 1.01, ""}
+
+	f := &Fortune{
+		Date:      DateFunc(func() time.Time { return test.date }),
+		Parameter: ParameterFunc(func() float64 { return test.parameter }),
+	}
+	got, err := f.Draw()
+	if err == nil {
+		t.Error("Expected Error but nil")
+	}
+	if test.want != got {
+		t.Errorf("Draw() => want%s, but got %s", test.want, got)
 	}
 }
 
@@ -39,6 +72,31 @@ func TestIsNewYear(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := isNewYear(tt.date); tt.want != got {
 				t.Errorf("isNewYear(%v) => want %t, but got %t", tt.date, tt.want, got)
+			}
+		})
+	}
+}
+
+func TestDraw(t *testing.T) {
+	cases := []struct {
+		name      string
+		parameter float64
+		want      string
+	}{
+		{"Low", 0.16, Low},
+		{"Middle", 0.5, Middle},
+		{"High", 0.83, High},
+		{"Great", 1, Great},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := draw(tt.parameter)
+			if err != nil {
+				t.Errorf("Unexpected Error : %v", err)
+			}
+			if tt.want != got {
+				t.Errorf("Draw() => want %s, but got %s", tt.want, got)
 			}
 		})
 	}
