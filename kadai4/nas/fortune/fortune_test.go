b7cb96a237e0864b4d5d9840cd974302ccbbf7da
nas <nas.silver.bullet@gmail.com>
2019-09-25 23:31:14 +0900

    テストを追加

--- kadai4/nas/fortune/fortune_test.go
+++ kadai4/nas/fortune/fortune_test.go
@@ -23,3 +23,23 @@ func TestDraw(t *testing.T) {
 		})
 	}
 }
+
+func TestIsNewYear(t *testing.T) {
+	cases := []struct {
+		name string
+		date time.Time
+		want bool
+	}{
+		{"True", time.Date(2019, 1, 1, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 60*60*9)), true},
+		{"FalseMonth", time.Date(2019, 2, 1, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 60*60*9)), false},
+		{"FalseDay", time.Date(2019, 1, 4, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 60*60*9)), false},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isNewYear(tt.date); tt.want != got {
+				t.Errorf("isNewYear(%v) => want %t, but got %t", tt.date, tt.want, got)
+			}
+		})
+	}
+}
