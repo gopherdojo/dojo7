8be1b61d75f79e74b208f4758bab2f9ad4998b10
nas <nas.silver.bullet@gmail.com>
2019-09-27 00:13:03 +0900

    テストを修正

--- kadai4/nas/fortune/main.go
+++ kadai4/nas/fortune/main.go
@@ -1,12 +1,37 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
+
+	"github.com/gopherdojo/dojo7/kadai4/nas/fortune/pkg/fortune"
+)
+
+var (
+	// MockDate is Date
+	MockDate fortune.Date
+	// MockParameter is Parameter
+	MockParameter fortune.Parameter
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, HTTPサーバ")
+	w.Header().Set("Content-Type", "applicaiton/json; charset=utf-8")
+
+	f := &fortune.Fortune{
+		Date:      MockDate,
+		Parameter: MockParameter,
+	}
+
+	l, err := f.Draw()
+	if err != nil {
+		http.Error(w, "Server error", http.StatusInternalServerError)
+		return
+	}
+
+	enc := json.NewEncoder(w)
+	if err := enc.Encode(l); err != nil {
+		http.Error(w, "Server error", http.StatusInternalServerError)
+	}
 }
 func main() {
 	http.HandleFunc("/", handler)
